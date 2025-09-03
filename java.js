efad8a19926db02a1c3535baf84a269c09384f28
Marisol-Przybysz <marisol.przybysz.ifpr.2023@gmail.com>
2025-09-03 16:13:01 -0300

    implementação do jogo

---
+++ java.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    const input = document.createElement("input");
+    input.type = "number";
+    input.id = "caixaNumero";
+    input.placeholder = "Digite um número...";
+
+    const container = document.getElementById("caixatxt");
+    container.appendChild(input);
+  
+};
+
+    const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    var na = numeroAleatorio;
+
+function armazenarValor() {
+  const valorTexto = document.getElementById("caixaNumero").value;
+  const n = Number(valorTexto);
+
+  if (n == na) {
+    document.getElementById("resultado").innerText = "Você acertou!";
+  }
+  if (n > na) {
+    document.getElementById("resultado").innerText = "Muito alto";
+  }
+  if (n < na) {
+    document.getElementById("resultado").innerText = "Muito baixo";
+  }
+}
